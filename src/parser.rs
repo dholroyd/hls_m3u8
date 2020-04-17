cb6007e33e735b87465c8fabb424f790ed2880e6
David Holroyd <dave@badgers-in-foil.co.uk>
2020-04-17 09:21:33 +0100

    Drop nom
    Just a plain-old recursive descent parser now.

--- src/parser.rs
+++ src/parser.rs
@@ -1,35 +1,24 @@
 use crate::{MediaPlaylist, Error, MediaSegment, tags};
-use nom::sequence::tuple;
-use nom::branch::alt;
-use nom::bytes::complete::{tag, is_a, take_while1, take_till, take_till1, take_until};
-use nom::combinator::{not, map, map_res, recognize, opt, peek};
-use nom::{IResult, CompareResult, InputLength};
-use nom::Compare;
-use nom::Slice;
 use crate::tags::{ExtInf, ExtXVersion, ExtXMediaSequence, ExtXIndependentSegments, ExtXTargetDuration, ExtXProgramDateTime, ExtXDateRange};
 use std::time::Duration;
 use crate::types::{ProtocolVersion, PlaylistType};
-use nom::error::{ErrorKind, ParseError};
 use std::convert::TryInto;
-use nom::bytes::streaming::take_while;
 use smallvec::alloc::borrow::Cow;
 use smallvec::alloc::string::FromUtf8Error;
 use std::str::FromStr;
-use nom::number::complete::double;
 use std::ops::RangeFrom;
-use nom::lib::std::convert::TryFrom;
 use std::fmt;
-use nom::lib::std::fmt::Formatter;
 use memchr::memchr;
+use std::io::BufRead;
 
 struct LazyStr<'a>(&'a[u8]);
 impl<'a> LazyStr<'a> {
-    fn try_to_string(&self) -> Result<String, std::string::FromUtf8Error> {
+    fn try_to_string(&self) -> std::result::Result<String, std::string::FromUtf8Error> {
         String::from_utf8(Vec::from(self.0))
     }
 }
 impl<'a> fmt::Debug for LazyStr<'a> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str(String::from_utf8_lossy(self.0).as_ref())
     }
 }
@@ -64,8 +53,19 @@ enum Tag<'a> {
     Unknown(&'a str),
 }
 
+#[derive(Debug)]
+pub enum ParseError<'a> {
+    Incomplete(&'static str),
+    Unexpected { expected: &'a [u8], found: &'a [u8] },
+    Utf8(&'a [u8]),
+    InvalidNumber,
+    ExpectedEndOfInput(&'a [u8]),
+    Attributes,
+}
+type Result<'a, T> = std::result::Result<(&'a [u8], T), ParseError<'a>>;
+
 enum SegmentState {
-    AraitExtinf,
+    AwaitExtinf,
     InSegment,
 }
 
@@ -79,7 +79,7 @@ struct MySegmentBuilder {
 impl Default for MySegmentBuilder {
     fn default() -> Self {
         MySegmentBuilder {
-            state: SegmentState::AraitExtinf,
+            state: SegmentState::AwaitExtinf,
             segments: vec![],
             program_date_time: None,
             date_range: None,
@@ -97,7 +97,7 @@ impl MySegmentBuilder {
             },
         };
         match self.state {
-            SegmentState::AraitExtinf => {
+            SegmentState::AwaitExtinf => {
                 println!("Got uri without EXTINF tag, {:?}", uri);
             },
             SegmentState::InSegment => {
@@ -107,7 +107,7 @@ impl MySegmentBuilder {
                 self.segments.push(seg);
             },
         }
-        self.state = SegmentState::AraitExtinf;
+        self.state = SegmentState::AwaitExtinf;
     }
     fn date_range(&mut self, date_range: ExtXDateRange) {
         self.date_range = Some(date_range);
@@ -123,7 +123,7 @@ impl MySegmentBuilder {
     }
 }
 
-struct MyPlaylistBuilder {
+pub struct MyPlaylistBuilder {
     version: Option<ProtocolVersion>,
     media_sequence: Option<usize>,
     target_duration: Option<Duration>,
@@ -183,7 +183,7 @@ impl MyPlaylistBuilder {
         }
     }
 
-    fn build(self) -> Result<MediaPlaylist, Error> {
+    pub fn build(self) -> std::result::Result<MediaPlaylist, Error> {
         Ok(MediaPlaylist {
             target_duration: self.target_duration.ok_or_else(|| Error::missing_tag("EXT-X-TARGETDURATION", ""))?,
             media_sequence: 0,
@@ -199,58 +199,54 @@ impl MyPlaylistBuilder {
         })
     }
 }
-pub fn nom_parser(input: &[u8]) -> Result<MediaPlaylist, Error> {
-    map(
-        nom::combinator::all_consuming(
-            tuple((
-                nom::bytes::complete::tag(b"#EXTM3U"),
-                nom::character::complete::line_ending,
-                lines,
-            ))
-        ),
-        |(_, _, p)| p,
-    )
-        (input)
-        .map_err(|e| {
-            match e {
-                nom::Err::Incomplete(e) => {
-                    println!("Incomplete {:?}", e);
-                },
-                nom::Err::Error((input, kind)) => {
-                    println!("Error {:?}", kind);
-                    println!("   {:?}", &String::from_utf8_lossy(input)[..100])
-                },
-                nom::Err::Failure(e) => {
-                    println!("Failure {:?}", e);
-                },
-            }
-            Error::invalid_input()
-        } )
-        .and_then(|(_, builder)| builder.build() )
-}
-fn lines(input: &[u8]) -> IResult<&[u8], MyPlaylistBuilder> {
-    let mut builder = MyPlaylistBuilder::default();
-    let mut iter = nom::combinator::iterator(input, hls_line);
-    for line in &mut iter {
-        builder.add_line(line);
+
+fn tag<'a>(input: &'a[u8], val: &'static [u8]) -> Result<'a, ()> {
+    if input.len() < val.len() {
+        return Err(ParseError::Incomplete("tag"))
     }
-    iter.finish().map(|(i, _)| (i, builder) )
+    if input.starts_with(val) {
+        Ok((&input[val.len()..], ()))
+    } else {
+        Err(ParseError::Unexpected { expected: val, found: &input[..val.len()] })
+    }
+}
+pub fn new_parser(input: &[u8]) -> std::result::Result<MyPlaylistBuilder, ParseError> {
+    let (input, _) = tag(input, b"#EXTM3U")?;
+    let (input, _) = line_ending(input)?;
+    let (input, builder) = lines(input)?;
+    if !input.is_empty() {
+        return Err(ParseError::ExpectedEndOfInput(input))
+    }
+    Ok(builder)
+}
+fn lines(mut input: &[u8]) -> Result<MyPlaylistBuilder> {
+    let mut builder = MyPlaylistBuilder::default();
+    loop {
+        if input.is_empty() {
+            return Ok((input, builder));
+        }
+        let (remain, line) = hls_line(input)?;
+        builder.add_line(line);
+        input = remain;
+    }
 }
 
-fn hls_line(input: &[u8]) -> IResult<&[u8], Line> {
-    let (input, _) = take_till(|c| c!=b' ' && c!=b'\t' )(input)?;
-
+fn hls_line(input: &[u8]) -> Result<Line> {
     let res = comment_or_tag(input);
     if res.is_ok() { return res; }
 
     let res = uri_line(input);
     if res.is_ok() { return res; }
 
-    map(peek(line_ending), |_| {println!("(blank)"); Line::Comment(LazyStr(b""))} )(input)  // TODO: blank line
+    if let Ok((line,_)) = line_ending(input) {
+        Ok((input, Line::Comment(LazyStr(b""))))  // TODO: blank line
+    } else {
+        res
+    }
 }
 
-fn comment_or_tag(input: &[u8]) -> IResult<&[u8], Line> {
-    let (input, _) = tag(b"#")(input)?;
+fn comment_or_tag(input: &[u8]) -> Result<Line> {
+    let (input, _) = tag(input, b"#")?;
 
     let res = ext_tag_eol(input).map(|(i, r)| (i, Line::Tag(r)) );
     if res.is_ok() { return res; }
@@ -258,7 +254,7 @@ fn comment_or_tag(input: &[u8]) -> IResult<&[u8], Line> {
     comment(input)
 }
 
-fn ext_tag_eol(input: &[u8]) -> IResult<&[u8], Tag> {
+fn ext_tag_eol(input: &[u8]) -> Result<Tag> {
     let (input, res) = ext_tag(input)?;
 
     if let Ok((trimmed_input, _)) = line_ending(input) {
@@ -267,8 +263,8 @@ fn ext_tag_eol(input: &[u8]) -> IResult<&[u8], Tag> {
         Ok((input, res))
     }
 }
-fn ext_tag(input: &[u8]) -> IResult<&[u8], Tag> {
-    let (input, _) = tag(b"EXT")(input)?;
+fn ext_tag(input: &[u8]) -> Result<Tag> {
+    let (input, _) = tag(input, b"EXT")?;
 
     let res = ext_inf(input).map(|(input, (duration, title))| (input, Tag::ExtInf { duration, title }));
     if res.is_ok() { return res; }
@@ -285,82 +281,67 @@ fn ext_tag(input: &[u8]) -> IResult<&[u8], Tag> {
     ext_target_duration(input).map(|(input, t)| (input, Tag::ExtXTargetDuration(t)))
 }
 
-fn ext_version(input: &[u8]) -> IResult<&[u8], ExtXVersion> {
-    map_res(
-        tuple((
-            tag(b"-X-VERSION:"),
-            take_till(|c| c==b'\r' || c==b'\n' ),
-        )),
-        |(_, t)| std::str::from_utf8(t)
-            .map_err(|_| nom::Err::Error((t, ErrorKind::Char)))
-            .and_then(|s| s.parse().map_err(|_| nom::Err::Error((t, ErrorKind::Digit))) )
-            .map(ExtXVersion::new)
-    )
-        (input)
+fn slice(input: &[u8], size: usize) -> Result<&[u8]> {
+    if size > input.len() {
+        Err(ParseError::Incomplete("slice"))
+    } else {
+        Ok((&input[size..], &input[..size]))
+    }
 }
 
-fn ext_media_sequence(input: &[u8]) -> IResult<&[u8], ExtXMediaSequence> {
-    map(
-        tuple((
-            tag(b"-X-MEDIA-SEQUENCE:"),
-            parse_u64,
-        )),
-        |(_, t)| ExtXMediaSequence(t as usize)
-    )
-        (input)
+fn utf8(input: &[u8]) -> std::result::Result<&str, ParseError> {
+    std::str::from_utf8(input)
+        .map_err(|_| ParseError::Utf8(input))
 }
 
-fn ext_independent_segments(input: &[u8]) -> IResult<&[u8], ExtXIndependentSegments> {
-    map(
-        tag(b"-X-INDEPENDENT-SEGMENTS"),
-        |_| ExtXIndependentSegments
-    )
-        (input)
+fn ext_version(input: &[u8]) -> Result<ExtXVersion> {
+    let (input, _) = tag(input, b"-X-VERSION:")?;
+    let (input, val) = slice(input, 1)?;
+    Ok((input, ExtXVersion::new(ProtocolVersion::from_str(utf8(val)?).map_err(|_| ParseError::Attributes)?)))
 }
 
-fn ext_target_duration(input: &[u8]) -> IResult<&[u8], ExtXTargetDuration> {
-    map(
-        tuple((
-            tag(b"-X-TARGETDURATION:"),
-            parse_u64,
-        )),
-        |(_, t)| ExtXTargetDuration(std::time::Duration::from_secs(t))
-    )
-        (input)
+fn ext_media_sequence(input: &[u8]) -> Result<ExtXMediaSequence> {
+    let (input, _) = tag(input, b"-X-MEDIA-SEQUENCE:")?;
+    let (input, msn) = parse_u64(input)?;
+    Ok((input, ExtXMediaSequence(msn as usize)))
 }
 
-fn ext_program_date_time(input: &[u8]) -> IResult<&[u8], ExtXProgramDateTime> {
-    map_res(
-        tuple((
-            tag(b"-X-PROGRAM-DATE-TIME:"),
-            take_till(|c| c==b'\r' || c==b'\n' ),
-        )),
-        |(_, t)| std::str::from_utf8(t)
-            .map_err(|_| nom::Err::Error((t, ErrorKind::Char)))
-            .map(ExtXProgramDateTime::new)
-    )
-        (input)
+fn ext_independent_segments(input: &[u8]) -> Result<ExtXIndependentSegments> {
+    let (input, _) = tag(input, b"-X-INDEPENDENT-SEGMENTS")?;
+    Ok((input, ExtXIndependentSegments))
 }
 
-fn ext_date_range(input: &[u8]) -> IResult<&[u8], ExtXDateRange> {
-    map_res(
-        tuple((
-            tag(b"-X-DATERANGE:"),
-            take_till(|c| c==b'\r' || c==b'\n' ),
-        )),
-        |(_, c)| std::str::from_utf8(c)
-            .map_err(|_| nom::Err::Error((c, ErrorKind::Char)))
-            .and_then(|s| ExtXDateRange::from_str_attrs(s).map_err(|e| nom::Err::Error((c, ErrorKind::Verify)) ) )
-    )
-        (input)
+fn ext_target_duration(input: &[u8]) -> Result<ExtXTargetDuration> {
+    let (input, _) = tag(input, b"-X-TARGETDURATION:")?;
+    let (input, val) = parse_u64(input)?;
+    Ok((input, ExtXTargetDuration(std::time::Duration::from_secs(val))))
 }
 
-fn comment(input: &[u8]) -> IResult<&[u8], Line> {
-    let (input, res) = map(
-        take_till(|c| c==b'\r' || c==b'\n' ),
-        |c| Line::Comment(LazyStr(b""))  // TODO
-    )
-        (input)?;
+fn take_till_eol(input: &[u8]) -> (&[u8], &[u8]) {
+    if let Some(pos) = memchr(b'\n', input) {
+        let (res, input) = input.split_at(pos);
+            (input, res)
+    } else {
+        (&input[0..0], input)
+    }
+}
+
+fn ext_program_date_time(input: &[u8]) -> Result<ExtXProgramDateTime> {
+    let (input, _) = tag(input, b"-X-PROGRAM-DATE-TIME:")?;
+    let (input, val) = take_till_eol(input);
+    Ok((input, utf8(val).map(ExtXProgramDateTime::new)?))
+}
+
+fn ext_date_range(input: &[u8]) -> Result<ExtXDateRange> {
+    let (input, _) = tag(input, b"-X-DATERANGE:")?;
+    let (input, val) = take_till_eol(input);
+    let res = utf8(val).and_then(|s| ExtXDateRange::from_str_attrs(s).map_err(|_| ParseError::Attributes))?;
+    Ok((input,  res))
+}
+
+fn comment(input: &[u8]) -> Result<Line> {
+    let (input, res) = take_till_eol(input);
+    let res = Line::Comment(LazyStr(res));
 
     if let Ok((trimmed_input, _)) = line_ending(input) {
         Ok((trimmed_input, res))
@@ -369,29 +350,25 @@ fn comment(input: &[u8]) -> IResult<&[u8], Line> {
     }
 }
 
-fn ext_inf(input: &[u8]) -> IResult<&[u8], (Duration, Option<LazyStr<'_>>)> {
-    let (input, _) = tag(b"INF:")(input)?;
+fn ext_inf(input: &[u8]) -> Result<(Duration, Option<LazyStr<'_>>)> {
+    let (input, _) = tag(input, b"INF:")?;
     let (input, duration) = duration(input)?;
-    let (input, _) = tag(b",")(input)?;
+    let (input, _) = tag(input, b",")?;
     let (input, description) = description(input)?;
 
     //inf.set_title_string(description);
     Ok((input, (duration, description)))
 }
 
-fn uri_line(input: &[u8]) -> IResult<&[u8], Line> {
+fn uri_line(input: &[u8]) -> Result<Line> {
+    let o = input.clone();
     if input.is_empty() {
-        return Err(nom::Err::Incomplete(nom::Needed::Size(1)))
+        return Err(ParseError::Incomplete("uri"))
     }
-    let (res, input) = if let Some(pos) = memchr(b'\n', input) {
-        if pos == 0 {
-            return Err(nom::Err::Incomplete(nom::Needed::Size(1)))
-        }
-        let (a, b) = input.split_at(pos);
-        (a, &b[1..])
-    } else {
-        (input, &input[0..0])
-    };
+    let (input, res) = take_till_eol(input);
+    if res.is_empty() {
+        return Err(ParseError::Incomplete("uri"))
+    }
     let res = if res.ends_with(b"\r") {
         &res[..res.len()-1]
     } else {
@@ -406,39 +383,45 @@ fn uri_line(input: &[u8]) -> IResult<&[u8], Line> {
 }
 
 
-fn duration(input: &[u8]) -> IResult<&[u8], Duration> {
-    double(input)
+fn duration(input: &[u8]) -> Result<Duration> {
+    parse_f64(input)
         .map(|(i, r)| (i, Duration::from_secs_f64(r)) )
 }
-fn description(input: &[u8]) -> IResult<&[u8], Option<LazyStr>> {
-    take_till(|c| c==b'\r' || c==b'\n' )(input)
-        .and_then(|(i, r)| {
-        if r.is_empty() {
-            Ok((i, None))
-        } else {
-            Ok((i, Some(LazyStr(r))))
-        }
-    })
+fn description(input: &[u8]) -> Result<Option<LazyStr>> {
+    let (i, r) = take_till_eol(input);
+    if r.is_empty() {
+        Ok((i, None))
+    } else {
+        Ok((i, Some(LazyStr(r))))
+    }
 }
 
 
-fn line_ending(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    match input.compare("\n") {
-        CompareResult::Ok => Ok((input.slice(1..), input.slice(0..1))),
-        CompareResult::Incomplete => IResult::Err(nom::Err::Error((input, ErrorKind::CrLf))),
-        CompareResult::Error => {
-            match input.compare("\r\n") {
-                //FIXME: is this the right index?
-                CompareResult::Ok => Ok((input.slice(2..), input.slice(0..2))),
-                _ => IResult::Err(nom::Err::Error((input, ErrorKind::CrLf))),
+fn line_ending(input: &[u8]) -> Result<()> {
+    if input.is_empty() {
+        Err(ParseError::Incomplete("line-end"))
+    } else {
+        if input.len() > 2 {
+            if let Ok((input, _)) = tag(input, b"\r\n") {
+                Ok((input, ()))
+            } else {
+                tag(input, b"\n")
             }
+        } else {
+            tag(input, b"\n")
         }
     }
 }
 
-fn parse_u64<'a, E:ParseError<&'a [u8]>>(input: &'a [u8]) -> IResult<&'a [u8], u64, E> {
+fn parse_u64(input: &[u8]) -> Result<u64> {
     match ::lexical_core::parse_partial(input) {
-        Ok((value, processed)) => Ok((input.slice(processed..), value)),
-        Err(_) => Err(nom::Err::Error(E::from_error_kind(input, ErrorKind::Digit)))
+        Ok((value, processed)) => Ok((&input[processed..], value)),
+        Err(_) => Err(ParseError::InvalidNumber)
+    }
+}
+fn parse_f64(input: &[u8]) -> Result<f64> {
+    match ::lexical_core::parse_partial(input) {
+        Ok((value, processed)) => Ok((&input[processed..], value)),
+        Err(_) => Err(ParseError::InvalidNumber)
     }
 }
